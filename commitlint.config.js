473b32cb1895b3eea0391570f589c9b73b9868df
bermuda <olasunkanmiboluwatife110@gmail.com>
2024-10-11 07:55:09 +0100

    commit setup

---
+++ commitlint.config.js
@@ -0,0 +1,14 @@
+/*
+ * ############################################################################### *
+ * Created Date: We Jul 2024                                                   *
+ * Author: Boluwatife Olasunkanmi O.                                           *
+ * -----                                                                       *
+ * Last Modified: Wed Jul 03 2024                                              *
+ * Modified By: Boluwatife Olasunkanmi O.                                      *
+ * -----                                                                       *
+ * HISTORY:                                                                    *
+ * Date      	By	Comments                                                   *
+ * ############################################################################### *
+ */
+
+module.exports = { extends: ['@commitlint/config-conventional'] };
